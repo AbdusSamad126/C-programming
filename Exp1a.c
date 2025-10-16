e93f679e97889d9100a48db66020c35295ba0812
AbdusSamad126 <abdussamadchowhan@eng.rizvi.edu.in>
2025-10-16 22:51:02 +0530

    Add files via upload

---
+++ Exp1a.c
@@ -0,0 +1,6 @@
+// Name:AbdusSamad Chowhan UIN:251P126 Computer Engineering D3 
+#include <stdio.h>
+int main(){
+    printf("Name :-AbdusSamad Chowhan \nAge:-18 \nClass :-F.E, \ndiv:- D \nUIN:-251P126\n");
+    return 0;
+}
