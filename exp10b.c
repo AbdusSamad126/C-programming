a7f7231cf6c551eca4de9f01eabb2305ecf2fe44
AbdusSamad126 <abdussamadchowhan@eng.rizvi.edu.in>
2025-12-02 22:09:34 +0530

    Add files via upload

---
+++ exp10b.c
@@ -0,0 +1,16 @@
+//Name :- Abdus Samad Chowhan
+//UID :- 251P126
+//Roll No:- 48
+//Branch :- Comp Eng
+//Div:- D
+#include <stdio.h>
+
+int main(){
+    int array[] = {10, 20, 30, 40, 50};
+    int n = sizeof(array) / sizeof(array[0]);
+    int *p = array;
+    for(int i = n - 1; i >= 0; i--) {
+        printf("%d ", *(p + i));
+    }
+    return 0;
+}
